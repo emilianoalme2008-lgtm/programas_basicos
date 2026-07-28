80674f1bbb4a7a49e68bf0de0dda8cfb80a3c6f6
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-27 21:06:15 -0600

    Create promedio_tres_numeros.py

---
+++ promedio_tres_numeros.py
@@ -0,0 +1,11 @@
+>>> # Programa para calcular el promedio de tres números
+... num1 = float(input("Número 1: "))
+... num2 = float(input("Número 2: "))
+... num3 = float(input("Número 3: "))
+... promedio = (num1 + num2 + num3) / 3
+... print("El promedio es:", promedio)
+...
+Número 1: 7
+Número 2: 4
+Número 3: 6
+El promedio es: 5.666666666666667
