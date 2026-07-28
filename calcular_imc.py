57a512606859cd9e1c2b4f27a574487eb08ce6b1
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-27 21:08:18 -0600

    Create calcular_imc.py

---
+++ calcular_imc.py
@@ -0,0 +1,9 @@
+# Programa para calcular el IMC
+... peso = float(input("Peso (kg): "))
+... altura = float(input("Altura (m): "))
+... imc = peso / (altura ** 2)
+... print("Su IMC es:", imc)
+...
+Peso (kg): 57
+Altura (m): 1.72
+Su IMC es: 19.2671714440238
