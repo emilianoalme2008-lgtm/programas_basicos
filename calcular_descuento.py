14893b70828c11ab8e3e8ed46edcf383bc725904
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-27 23:33:12 -0600

    Create calcular_descuento.py

---
+++ calcular_descuento.py
@@ -0,0 +1,63 @@
+>>> #Calcular precio con descuento
+... precio = float(input("Precio original: "))
+... if precio <= 100:
+...     descuento = 0
+... elif precio <= 200:
+...     descuento = 0.10
+... elif precio <= 500:
+...     descuento = 0.20
+... else:
+...     descuento = 0.20
+... precio_final = precio - (precio * descuento)
+... print("Precio con descuento:", precio_final)
+...
+Precio original: 50
+Precio con descuento: 50.0
+>>>
+>>> #Calcular precio con descuento
+... precio = float(input("Precio original: "))
+... if precio <= 100:
+...     descuento = 0
+... elif precio <= 200:
+...     descuento = 0.10
+... elif precio <= 500:
+...     descuento = 0.20
+... else:
+...     descuento = 0.20
+... precio_final = precio - (precio * descuento)
+... print("Precio con descuento:", precio_final)
+...
+Precio original: 150
+Precio con descuento: 135.0
+>>>
+>>> #Calcular precio con descuento
+... precio = float(input("Precio original: "))
+... if precio <= 100:
+...     descuento = 0
+... elif precio <= 200:
+...     descuento = 0.10
+... elif precio <= 500:
+...     descuento = 0.20
+... else:
+...     descuento = 0.20
+... precio_final = precio - (precio * descuento)
+... print("Precio con descuento:", precio_final)
+...
+Precio original: 300
+Precio con descuento: 240.0
+>>>
+>>> #Calcular precio con descuento
+... precio = float(input("Precio original: "))
+... if precio <= 100:
+...     descuento = 0
+... elif precio <= 200:
+...     descuento = 0.10
+... elif precio <= 500:
+...     descuento = 0.20
+... else:
+...     descuento = 0.20
+... precio_final = precio - (precio * descuento)
+... print("Precio con descuento:", precio_final)
+...
+Precio original: 600
+Precio con descuento: 480.0
