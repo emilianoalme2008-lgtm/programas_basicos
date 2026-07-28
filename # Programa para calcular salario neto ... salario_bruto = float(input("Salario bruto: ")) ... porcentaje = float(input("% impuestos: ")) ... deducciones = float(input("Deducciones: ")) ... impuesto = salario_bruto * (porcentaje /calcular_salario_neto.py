705f7abc560179c44948630e31b746b622186a7b
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-27 21:09:25 -0600

    Create calcular_salario_neto.py

---
+++ # Programa para calcular salario neto ... salario_bruto = float(input("Salario bruto: ")) ... porcentaje = float(input("% impuestos: ")) ... deducciones = float(input("Deducciones: ")) ... impuesto = salario_bruto * (porcentaje /calcular_salario_neto.py
@@ -0,0 +1,12 @@
+ # Programa para calcular salario neto
+... salario_bruto = float(input("Salario bruto: "))
+... porcentaje = float(input("% impuestos: "))
+... deducciones = float(input("Deducciones: "))
+... impuesto = salario_bruto * (porcentaje / 100)
+... salario_neto = salario_bruto - impuesto - deducciones
+... print("Salario neto:", salario_neto)
+...
+Salario bruto: 10000
+% impuestos: 15
+Deducciones: 500
+Salario neto: 8000.0
