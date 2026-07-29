d394b86520e06cb2c16e0fd2378f910cfe84a847
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-28 19:46:44 -0600

    Create estaciones_del_año.py

---
+++ estaciones_del_año.py
@@ -0,0 +1,90 @@
+>>> #Estaciones del año
+... mes = int(input("Número de mes (1-12): "))
+... match mes:
+...     case 12 | 1 | 2:
+...         estacion = "Invierno"
+...     case 3 | 4 | 5:
+...         estacion = "Primavera"
+...     case 6 | 7 | 8:
+...         estacion = "Verano"
+...     case 9 | 10 | 11:
+...         estacion = "Otoño"
+...     case _:
+...         estacion = "Mes inválido"
+... print("Estación:", estacion)
+...
+Número de mes (1-12): 2
+Estación: Invierno
+>>>
+>>> #Estaciones del año
+... mes = int(input("Número de mes (1-12): "))
+... match mes:
+...     case 12 | 1 | 2:
+...         estacion = "Invierno"
+...     case 3 | 4 | 5:
+...         estacion = "Primavera"
+...     case 6 | 7 | 8:
+...         estacion = "Verano"
+...     case 9 | 10 | 11:
+...         estacion = "Otoño"
+...     case _:
+...         estacion = "Mes inválido"
+... print("Estación:", estacion)
+...
+Número de mes (1-12): 4
+Estación: Primavera
+>>>
+>>>
+>>> #Estaciones del año
+... mes = int(input("Número de mes (1-12): "))
+... match mes:
+...     case 12 | 1 | 2:
+...         estacion = "Invierno"
+...     case 3 | 4 | 5:
+...         estacion = "Primavera"
+...     case 6 | 7 | 8:
+...         estacion = "Verano"
+...     case 9 | 10 | 11:
+...         estacion = "Otoño"
+...     case _:
+...         estacion = "Mes inválido"
+... print("Estación:", estacion)
+...
+Número de mes (1-12): 6
+Estación: Verano
+>>>
+>>> #Estaciones del año
+... mes = int(input("Número de mes (1-12): "))
+... match mes:
+...     case 12 | 1 | 2:
+...         estacion = "Invierno"
+...     case 3 | 4 | 5:
+...         estacion = "Primavera"
+...     case 6 | 7 | 8:
+...         estacion = "Verano"
+...     case 9 | 10 | 11:
+...         estacion = "Otoño"
+...     case _:
+...         estacion = "Mes inválido"
+... print("Estación:", estacion)
+...
+Número de mes (1-12): 9
+Estación: Otoño
+>>>
+>>> #Estaciones del año
+... mes = int(input("Número de mes (1-12): "))
+... match mes:
+...     case 12 | 1 | 2:
+...         estacion = "Invierno"
+...     case 3 | 4 | 5:
+...         estacion = "Primavera"
+...     case 6 | 7 | 8:
+...         estacion = "Verano"
+...     case 9 | 10 | 11:
+...         estacion = "Otoño"
+...     case _:
+...         estacion = "Mes inválido"
+... print("Estación:", estacion)
+...
+Número de mes (1-12): 12
+Estación: Invierno
