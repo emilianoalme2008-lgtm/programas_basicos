e1e47a5556930272e835647af824accd11191363
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-28 19:48:55 -0600

    Create votar_edad.py

---
+++ votar_edad.py
@@ -0,0 +1,29 @@
+>>> #Verificar edad
+... edad = int(input("¿Cuál es tu edad? "))
+... if edad >= 18:
+...     print("Si puede votar")
+... else:
+...     print("No puede votar")
+...
+¿Cuál es tu edad? 17
+No puede votar
+>>>
+>>> #Verificar edad
+... edad = int(input("¿Cuál es tu edad? "))
+... if edad >= 18:
+...     print("Si puede votar")
+... else:
+...     print("No puede votar")
+...
+¿Cuál es tu edad? 18
+Si puede votar
+>>>
+>>> #Verificar edad
+... edad = int(input("¿Cuál es tu edad? "))
+... if edad >= 18:
+...     print("Si puede votar")
+... else:
+...     print("No puede votar")
+...
+¿Cuál es tu edad? 25
+Si puede votar
