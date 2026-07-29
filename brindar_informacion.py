c5c660b9c2f00dd49800988d9f9d358c8982cb8e
emilianoalme2008-lgtm <emilianoalme2008@gmail.com>
2026-07-28 19:55:21 -0600

    Create brindar_informacion.py

---
+++ brindar_informacion.py
@@ -0,0 +1,119 @@
+>>> #Brindar información
+... consulta = input("Ingrese nombre de artista, película o serie: ").lower()
+... match consulta:
+...     case "inception":
+...         info = "Pelicula de ciencia ficción dirigida por Christopher Nolan."
+...     case "beatles":
+...         info = "Banda británica de rock formada en 1960."
+...     case "rick and morty":
+...         info = "Serie animada de comedia y ciencia ficción."
+...     case "stranger things":
+...         info = "Serie de terror y ciencia ficción de Netflix."
+...     case "avengers":
+...         info = "Pelicula de superhéroes del MCU."
+...     case _:
+...         info = "No se encontró información."
+... print("Información:", info)
+...
+Ingrese nombre de artista, película o serie: inception
+Información: Pelicula de ciencia ficción dirigida por Christopher Nolan.
+>>>
+>>> #Brindar información
+... consulta = input("Ingrese nombre de artista, película o serie: ").lower()
+... match consulta:
+...     case "inception":
+...         info = "Pelicula de ciencia ficción dirigida por Christopher Nolan."
+...     case "beatles":
+...         info = "Banda británica de rock formada en 1960."
+...     case "rick and morty":
+...         info = "Serie animada de comedia y ciencia ficción."
+...     case "stranger things":
+...         info = "Serie de terror y ciencia ficción de Netflix."
+...     case "avengers":
+...         info = "Pelicula de superhéroes del MCU."
+...     case _:
+...         info = "No se encontró información."
+... print("Información:", info)
+...
+Ingrese nombre de artista, película o serie: beatles
+Información: Banda británica de rock formada en 1960.
+>>>
+>>> #Brindar información
+... consulta = input("Ingrese nombre de artista, película o serie: ").lower()
+... match consulta:
+...     case "inception":
+...         info = "Pelicula de ciencia ficción dirigida por Christopher Nolan."
+...     case "beatles":
+...         info = "Banda británica de rock formada en 1960."
+...     case "rick and morty":
+...         info = "Serie animada de comedia y ciencia ficción."
+...     case "stranger things":
+...         info = "Serie de terror y ciencia ficción de Netflix."
+...     case "avengers":
+...         info = "Pelicula de superhéroes del MCU."
+...     case _:
+...         info = "No se encontró información."
+... print("Información:", info)
+...
+Ingrese nombre de artista, película o serie: rick and morty
+Información: Serie animada de comedia y ciencia ficción.
+>>>
+>>> #Brindar información
+... consulta = input("Ingrese nombre de artista, película o serie: ").lower()
+... match consulta:
+...     case "inception":
+...         info = "Pelicula de ciencia ficción dirigida por Christopher Nolan."
+...     case "beatles":
+...         info = "Banda británica de rock formada en 1960."
+...     case "rick and morty":
+...         info = "Serie animada de comedia y ciencia ficción."
+...     case "stranger things":
+...         info = "Serie de terror y ciencia ficción de Netflix."
+...     case "avengers":
+...         info = "Pelicula de superhéroes del MCU."
+...     case _:
+...         info = "No se encontró información."
+... print("Información:", info)
+...
+Ingrese nombre de artista, película o serie: strangers things
+Información: No se encontró información.
+>>>
+>>> #Brindar información
+... consulta = input("Ingrese nombre de artista, película o serie: ").lower()
+... match consulta:
+...     case "inception":
+...         info = "Pelicula de ciencia ficción dirigida por Christopher Nolan."
+...     case "beatles":
+...         info = "Banda británica de rock formada en 1960."
+...     case "rick and morty":
+...         info = "Serie animada de comedia y ciencia ficción."
+...     case "stranger things":
+...         info = "Serie de terror y ciencia ficción de Netflix."
+...     case "avengers":
+...         info = "Pelicula de superhéroes del MCU."
+...     case _:
+...         info = "No se encontró información."
+... print("Información:", info)
+...
+Ingrese nombre de artista, película o serie: stranger things
+Información: Serie de terror y ciencia ficción de Netflix.
+>>>
+>>> #Brindar información
+... consulta = input("Ingrese nombre de artista, película o serie: ").lower()
+... match consulta:
+...     case "inception":
+...         info = "Pelicula de ciencia ficción dirigida por Christopher Nolan."
+...     case "beatles":
+...         info = "Banda británica de rock formada en 1960."
+...     case "rick and morty":
+...         info = "Serie animada de comedia y ciencia ficción."
+...     case "stranger things":
+...         info = "Serie de terror y ciencia ficción de Netflix."
+...     case "avengers":
+...         info = "Pelicula de superhéroes del MCU."
+...     case _:
+...         info = "No se encontró información."
+... print("Información:", info)
+...
+Ingrese nombre de artista, película o serie: avengers
+Información: Pelicula de superhéroes del MCU.
